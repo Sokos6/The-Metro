b4df620d47818365fa36aac7086a1adaecf2dde5
Sokos6 <will.sokolowski@gmail.com>
2019-12-15 21:20:01 -0600

    moving metro

--- src/Metronome.js
+++ src/Metronome.js
@@ -20,8 +20,20 @@ class Metronome extends Component {
 
   handleBpmChange = event => {
     const bpm = event.target.value;
-    this.setState({ bpm });
+
+    if (this.state.playing) {
+      clearInterval(this.timer);
+      this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
+
+      this.setState({
+        count: 0,
+        bpm
+      });
+    } else {
+      this.setState({ bpm });
+    }
   }
+
   startStop = () => {
     if (this.state.playing) {
       clearInterval(this.timer);
